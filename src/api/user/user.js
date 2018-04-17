289208366cf9558b6fda285373c90fbf3b761356
duduneto <31359405+duduneto@users.noreply.github.com>
2018-04-17 03:31:47 -0300

    Campo Adm adicionado ao Usuário

--- src/api/user/user.js
+++ src/api/user/user.js
@@ -4,7 +4,8 @@ const mongoose = restful.mongoose;
 const userSchema = new mongoose.Schema({ 
     name: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String, min: 6, max: 12, required: true }
+    password: { type: String, min: 6, max: 12, required: true },
+    adm: {type: Boolean, default: false }
 });
 
 module.exports = restful.model('User', userSchema);
